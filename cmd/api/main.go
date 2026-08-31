251446095af9bea3c04e7342edfdf233471325f7
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-08-31 11:28:01 +0300

    feature/user: implement server initialization in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/chuuch/gorest/internal/config"
+	"github.com/chuuch/gorest/internal/server"
 )
 
 func main() {
-	fmt.Println("Started!")
+	cfg, err := config.Load()
+	if err != nil {
+		slog.Error("load configuration", "error", err)
+		os.Exit(1)
+	}
+
+	srv, err := server.New(cfg)
+	if err != nil {
+		slog.Error("initialize server", "error", err)
+		os.Exit(1)
+	}
+
+	if err := srv.Run(*cfg); err != nil {
+		slog.Error("server stopped with error", "error", err)
+		os.Exit(1)
+	}
 }
